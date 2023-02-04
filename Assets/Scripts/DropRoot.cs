10d897b32af60318074ffecf7d4b54794724eebd
Mathias Haimerl <mathias.haimerl@thi.de>
2023-02-04 18:04:13 +0100

    fuyouh!

--- Assets/Scripts/DropRoot.cs
+++ Assets/Scripts/DropRoot.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using HTC.UnityPlugin.Vive;
 using UnityEngine;
 
 public class DropRoot : MonoBehaviour
@@ -19,6 +20,7 @@ public class DropRoot : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (!collision.gameObject.CompareTag("Root")) return;
+        collision.gameObject.GetComponent<BasicGrabbable>().ForceRelease();
         Destroy(collision.gameObject);
         effectFire.SetActive(true);
         rendererMat.color = Random.ColorHSV();
